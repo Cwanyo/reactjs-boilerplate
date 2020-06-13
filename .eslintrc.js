bdfe51728c8a1052e7fb3cff34560a230456eefb
Cwanyo <manwan_444@hotmail.com>
2020-06-13 22:45:29 +0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const path = require('path')
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
+)
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'react', 'react-hooks', 'jsx-a11y'],
+  env: {
+    jest: true,
+    browser: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'arrow-parens': ['error', 'always'],
+    semi: ['error', 'never'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'function-paren-newline': ['error', 'consistent'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 0,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+    'no-console': 1,
+    'react/jsx-filename-extension': 0,
+  },
+}
